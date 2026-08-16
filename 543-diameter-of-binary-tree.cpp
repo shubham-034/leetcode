e75ade30011594321c28a0f7c7da6c1ddd52ea91
shubham-034 <142326352+shubham-034@users.noreply.github.com>
2026-08-16 09:58:30 +0530

    543 Diameter of Binary Tree - Runtime: 0 ms, Memory: 23.7 MB

---
+++ 543-diameter-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int leftHeight=height(root->left);
+        int rightHeight=height(root->right);
+
+        ans=max((leftHeight+rightHeight),ans);//current diameter of root node
+
+        return max(leftHeight,rightHeight)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return ans;
+    }
+};
